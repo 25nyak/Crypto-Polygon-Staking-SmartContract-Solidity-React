c210b7524c52bcf728cb51d722cd199cc1c3e76f
25nyak <56729621+25nyak@users.noreply.github.com>
2024-07-24 12:19:16 -0500

    Update web3API.js

--- src/utils/web3API.js
+++ src/utils/web3API.js
@@ -1,27 +1,27 @@
-import Web3 from 'web3'
-import contractAbi from '../config/abi.json'
+import Web3 from 'web3';
+import contractAbi from '../config/abi.json';
 import { toast } from 'react-toastify';
 
 let Contract = require("web3-eth-contract");
-Contract.setProvider("https://rpc-mainnet.maticvigil.com")
-export const connectToWallet = async () => {//connect the metamask using web3 and get current user account
+Contract.setProvider("https://rpc-mainnet.maticvigil.com");
+
+export const connectToWallet = async () => {
     let web3;
     try {
         if (window.ethereum) {
-            web3 = new Web3(window.ethereum)
-            await window.ethereum.request({ method: 'eth_requestAccounts' })
+            web3 = new Web3(window.ethereum);
+            await window.ethereum.request({ method: 'eth_requestAccounts' });
         } else if (window.web3) {
-            web3 = new Web3(window.web3.currentProvider || "https://rpc-mainnet.maticvigil.com")
+            web3 = new Web3(window.web3.currentProvider || "https://rpc-mainnet.maticvigil.com");
         }
         const accounts = await web3.eth.getAccounts();
-        return accounts[0]
+        return accounts[0];
     } catch (error) {
-        // console.log("Error: ", error)
-        return false
+        return false;
     }
-}
+};
 
-export const getWeb3 = async () => {//first connect the EVM and return web3 instance. always use!
+export const getWeb3 = async () => {
     let web3;
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
@@ -33,7 +33,7 @@ export const getWeb3 = async () => {//first connect the EVM and return web3 inst
         return false;
     }
     return web3;
-}
+};
 
 export const web3_contact_balance = async () => {
     const web3 = await getWeb3();
@@ -42,17 +42,15 @@ export const web3_contact_balance = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
-        const res = await extraMaticContract.methods.getSiteInfo().call()
-        let contact_balance = web3.utils.fromWei(res['_contractBalance'])
-        // console.log("contract_balance" + res['_contractBalance'])
-        return contact_balance
-       
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
+        const res = await extraMaticContract.methods.getSiteInfo().call();
+        let contact_balance = web3.utils.fromWei(res['_contractBalance']);
+        return contact_balance;
     } catch (e) {
-        console.log("failed")
+        console.log("Failed to fetch contract balance");
         return false;
     }
-}
+};
 
 export const web3_total_deposit = async () => {
     const web3 = await getWeb3();
@@ -61,16 +59,14 @@ export const web3_total_deposit = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
-        const res = await extraMaticContract.methods.getSiteInfo().call()//get total staked money value
-        let total_deposit = web3.utils.fromWei(res['_totalInvested'])
-        // console.log("total_deposit" + res['_totalInvested'])
-        return total_deposit 
-       
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
+        const res = await extraMaticContract.methods.getSiteInfo().call();
+        let total_deposit = web3.utils.fromWei(res['_totalInvested']);
+        return total_deposit;
     } catch (e) {
         return false;
     }
-}
+};
 
 export const web3_user_deposit_count = async () => {
     const web3 = await getWeb3();
@@ -79,17 +75,14 @@ export const web3_user_deposit_count = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const accounts = await web3.eth.getAccounts();
-        const res = await extraMaticContract.methods.getUserAmountOfDeposits(accounts[0]).call()//get total staked money value
-        // let contact_balance = web3.utils.fromWei(res)
-        
-        return res 
-       
+        const res = await extraMaticContract.methods.getUserAmountOfDeposits(accounts[0]).call();
+        return res;
     } catch (e) {
         return false;
     }
-}
+};
 
 export const web3_user_total_deposit = async () => {
     const web3 = await getWeb3();
@@ -98,16 +91,15 @@ export const web3_user_total_deposit = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const accounts = await web3.eth.getAccounts();
-        const res = await extraMaticContract.methods.getUserInfo(accounts[0]).call()//get total staked money value
-        let contact_balance = web3.utils.fromWei(res['totalDeposit'])
-        return contact_balance 
-       
+        const res = await extraMaticContract.methods.getUserInfo(accounts[0]).call();
+        let total_deposit = web3.utils.fromWei(res['totalDeposit']);
+        return total_deposit;
     } catch (e) {
         return false;
     }
-}
+};
 
 export const web3_user_total_withdraw = async () => {
     const web3 = await getWeb3();
@@ -116,16 +108,15 @@ export const web3_user_total_withdraw = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const accounts = await web3.eth.getAccounts();
-        const res = await extraMaticContract.methods.getUserInfo(accounts[0]).call()//get total staked money value
-        let contact_balance = web3.utils.fromWei(res['totalWithdrawn'])
-        return contact_balance 
-       
+        const res = await extraMaticContract.methods.getUserInfo(accounts[0]).call();
+        let total_withdraw = web3.utils.fromWei(res['totalWithdrawn']);
+        return total_withdraw;
     } catch (e) {
         return false;
     }
-}
+};
 
 export const web3_user_referral_bonus = async () => {
     const web3 = await getWeb3();
@@ -134,16 +125,15 @@ export const web3_user_referral_bonus = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const accounts = await web3.eth.getAccounts();
-        const res = await extraMaticContract.methods.getUserReferralTotalBonus(accounts[0]).call()//get total staked money value
-        let contact_balance = web3.utils.fromWei(res)
-        return contact_balance 
-       
+        const res = await extraMaticContract.methods.getUserReferralTotalBonus(accounts[0]).call();
+        let referral_bonus = web3.utils.fromWei(res);
+        return referral_bonus;
     } catch (e) {
         return false;
     }
-}
+};
 
 export const web3_referred_users = async () => {
     const web3 = await getWeb3();
@@ -152,16 +142,14 @@ export const web3_referred_users = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const accounts = await web3.eth.getAccounts();
-        const res = await extraMaticContract.methods.getUserTotalReferrals(accounts[0]).call()//get total staked money value
-        // let contact_balance = web3.utils.fromWei(res)
-        return res 
-       
+        const res = await extraMaticContract.methods.getUserTotalReferrals(accounts[0]).call();
+        return res;
     } catch (e) {
         return false;
     }
-}
+};
 
 export const web3_available_balance = async () => {
     const web3 = await getWeb3();
@@ -170,18 +158,17 @@ export const web3_available_balance = async () => {
         return false;
     }
     try {
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );//create instance of contract with abi and address
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const accounts = await web3.eth.getAccounts();
-        const res = await extraMaticContract.methods.getUserAvailable(accounts[0]).call()//get total staked money value
-        let contact_balance = web3.utils.fromWei(res)
-        return contact_balance 
-       
+        const res = await extraMaticContract.methods.getUserAvailable(accounts[0]).call();
+        let available_balance = web3.utils.fromWei(res);
+        return available_balance;
     } catch (e) {
         return false;
     }
-}
+};
 
-export const stakeMATIC = async (referral_address , amount, planId) => {//stake amount of BNB to the planId's plan
+export const stakeMATIC = async (referral_address, amount, planId) => {
     const web3 = await getWeb3();
     if (!web3) {
         toast.error("No web3 instance found.");
@@ -189,33 +176,26 @@ export const stakeMATIC = async (referral_address , amount, planId) => {//stake 
     }
     try {
         let connectedAddress = await connectToWallet();
-        console.log(connectedAddress);
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const txCount = await web3.eth.getTransactionCount(connectedAddress);
-        console.log(referral_address);
-        if(!referral_address)
-        var referrer = "0x2D19c11f66BE26Ba13333C428aD2050630B8176b"
-        else referrer = referral_address
-        console.log(referrer);
-        const myNewData = await extraMaticContract.methods.invest(referrer, planId).encodeABI()
+        let referrer = referral_address || "0x2D19c11f66BE26Ba13333C428aD2050630B8176b";
+        const myNewData = await extraMaticContract.methods.invest(referrer, planId).encodeABI();
         let weiPrice = web3.utils.toWei(`${amount}`, 'ether');
-        const gas2 = await web3.eth.getGasPrice()
+        const gas2 = await web3.eth.getGasPrice();
         const transactionParameters = {
             nonce: web3.utils.toHex(txCount),
             gasPrice: web3.utils.toHex(gas2),
-            // gasLimit: web3.utils.toHex(gasLimit),
-            to: "0x2a2cf93bc92537a596e7956315ce914186d0242f" ,
+            to: "0x2a2cf93bc92537a596e7956315ce914186d0242f",
             from: connectedAddress,
             data: myNewData,
             value: web3.utils.toHex(weiPrice)
-        }
+        };
 
-        // As with any RPC call, it may throw an error
         const txHash = await window.ethereum.request({
             method: 'eth_sendTransaction',
             params: [transactionParameters],
         });
-        
+
         if (txHash) {
             console.log("Transaction Done Successfully.");
         }
@@ -223,41 +203,33 @@ export const stakeMATIC = async (referral_address , amount, planId) => {//stake 
         toast.error(e.message);
         return false;
     }
-}
+};
 
 export const reinvest = async (planId) => {
     const web3 = await getWeb3();
-    const amount = '0.1';
-    var referrer = "0x2D19c11f66BE26Ba13333C428aD2050630B8176b"
     if (!web3) {
         toast.error("No web3 instance found.");
         return false;
     }
     try {
         let connectedAddress = await connectToWallet();
-        console.log(connectedAddress);
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
         const txCount = await web3.eth.getTransactionCount(connectedAddress);
-        const myNewData = await extraMaticContract.methods.reinvest(planId).encodeABI()
-        console.log(myNewData);
-        let weiPrice = web3.utils.toWei(`${amount}`, 'ether');
-        const gas2 = await web3.eth.getGasPrice()
+        const myNewData = await extraMaticContract.methods.reinvest(planId).encodeABI();
+        const gas2 = await web3.eth.getGasPrice();
         const transactionParameters = {
             nonce: web3.utils.toHex(txCount),
             gasPrice: web3.utils.toHex(gas2),
-            // gasLimit: web3.utils.toHex(gasLimit),
-            to: "0x2a2cf93bc92537a596e7956315ce914186d0242f" ,
+            to: "0x2a2cf93bc92537a596e7956315ce914186d0242f",
             from: connectedAddress,
             data: myNewData,
-            // value: web3.utils.toHex(weiPrice)
-        }
+        };
 
-        // As with any RPC call, it may throw an error
         const txHash = await window.ethereum.request({
             method: 'eth_sendTransaction',
             params: [transactionParameters],
         });
-        
+
         if (txHash) {
             console.log("Transaction Done Successfully.");
         }
@@ -265,7 +237,7 @@ export const reinvest = async (planId) => {
         console.log(e.message);
         return false;
     }
-}
+};
 
 export const WithdrawFn = async () => {
     const web3 = await getWeb3();
@@ -275,29 +247,28 @@ export const WithdrawFn = async () => {
     }
     try {
         let connectedAddress = await connectToWallet();
-        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f" );
-        const txCount = await web3.eth.getTransactionCount(connectedAddress);//get total trasaction count sent by current address
-        const myNewData = await extraMaticContract.methods.withdraw().encodeABI()
-        const gas2 = await web3.eth.getGasPrice()
+        let extraMaticContract = new Contract(contractAbi, "0x2a2cf93bc92537a596e7956315ce914186d0242f");
+        const txCount = await web3.eth.getTransactionCount(connectedAddress);
+        const myNewData = await extraMaticContract.methods.withdraw().encodeABI();
+        const gas2 = await web3.eth.getGasPrice();
         const transactionParameters = {
             nonce: web3.utils.toHex(txCount),
             gasPrice: web3.utils.toHex(gas2),
-            // gasLimit: web3.utils.toHex(gasLimit),
             to: "0x2a2cf93bc92537a596e7956315ce914186d0242f",
             from: connectedAddress,
             data: myNewData,
-        }
-        // As with any RPC call, it may throw an error
+        };
+
         const txHash = await window.ethereum.request({
             method: 'eth_sendTransaction',
             params: [transactionParameters],
         });
+
         if (txHash) {
             toast.success("Transaction Done Successfully.");
-            return
         }
     } catch (e) {
         toast.error(e.message);
         return false;
     }
-}
+};
